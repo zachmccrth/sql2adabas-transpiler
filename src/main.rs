aa106c6bc80f4292e31fe9892be8da0a75762d83
anoborame <zacharymccrth@gmail.com>
2025-05-15 15:41:42 -0400

    Add datafusion-sqlparser.rs, generic tutorial script.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser;
 fn main() {
-    println!("Hello, world!");
+    let sql = "SELECT a, b, 123, myfunc(b) \
+           FROM table_1 \
+           WHERE a > b AND b < 100 \
+           ORDER BY a DESC, b";
+
+    let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
+
+    let ast = Parser::parse_sql(&dialect, sql).unwrap();
+
+    println!("AST: {:?}", ast);
 }
