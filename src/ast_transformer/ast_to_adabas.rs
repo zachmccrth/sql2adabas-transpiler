afa3ebffd5bab6ced4488b951b798430c1f1a379
anoborame <zacharymccrth@gmail.com>
2025-05-16 15:02:53 -0400

    Refactor method signature to Result<Optional<>,Error> for the case that there is no where statement.

--- src/ast_transformer/ast_to_adabas.rs
+++ src/ast_transformer/ast_to_adabas.rs
@@ -1,24 +1,33 @@
 use sqlparser::ast::{Expr, Query, SetExpr};
 use sqlparser::parser::{ParserError};
 
-pub fn ast_to_adabas(ast: &Vec<&Query>) -> Result<Expr, ParserError> {
-    let thing: &Query = ast.get(0).unwrap();
-    let select = match thing.body.as_ref() {
+pub fn ast_to_adabas(select_statement_ast: &Query) -> Result<Option<Expr>, ParserError> {
+    let where_clause = match select_statement_ast.body.as_ref() {
         SetExpr::Select(statement) => {
            &statement.selection
         }
-        _ => {panic!("Not a select!")} 
+        _ => {return Err(ParserError::ParserError(format!("Not a select statement")))} 
     };
-    match select {
-        None => {Err(ParserError::ParserError(format!("No select!")))},
-        Some(valid) => {retrieve_tree(valid)}
+    match where_clause {
+        // This is a bit weird, but basically it might be possible to not have a where condition,
+        // which means that it is a valid parse but there is nothing to return (therefore an 
+        // optional)
+        None => {Ok(None)},
+        Some(valid) => {match retrieve_tree(valid) {
+            Ok(expression) => {Ok(Some(expression))}
+            Err(errortype) => {Err(errortype)}
+        }}
     }
 }
 // Ok what do we want to do here?
-// I'd like to be able to define the tree. That   means I need to know what a WHERE clause turns into
-// Ok. After examination, the parser will literally accept any expression after a where, it would be my responsibility to ensure
-// that only valid ADABAS expressions are accepted (honestly a feature, thank you package authors)
+// I'd like to be able to define the tree. That means I need to know what a WHERE clause turns into
+// Ok. After examination, the parser will literally accept any expression after a where, it would be
+// my responsibility to ensure that only valid ADABAS expressions are accepted 
+// (honestly a feature, thank you package authors)
+
+// Here there is definitely and expression, just might be a parser error
 fn retrieve_tree(expression: &Expr) -> Result<Expr, ParserError> {
+    // Based on Rust docs, recursive types require a Box
     match  expression {
         Expr::BinaryOp { op, left, right } => {
            Ok(Expr::BinaryOp {op: op.clone(), left: left.clone(), right: right.clone()}) 
